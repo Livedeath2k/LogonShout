7173938f72668d57dcf30fdf64a1c7a592a60fc4
Livedeath2k <Livedeath2k@users.noreply.github.com>
2018-05-30 10:45:37 +0200

    New Try

--- de/logonswear.ps1
+++ de/logonswear.ps1
@@ -1,10 +1,12 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 Add-Type -AssemblyName System.speech
 $Speech1, $Speech2, $Speech3 = 'Hallo Du ', 'Willkommen ', 'Tschüss '
-$word = gc c:\LogonSwear\liste.txt | sort{get-random} | select -First 1
+$word = gc $scriptPath\list.txt | sort{get-random} | select -First 1
 If ($Art = 1) {$SpeechA = "$Speech1 $Word"}
 ElseIf ($Art = 2) {$SpeechA = "$Speech2 $Word"}
 ElseIf ($Art = 3) {$SpeechA = "$Speech3 $Word"}
-End If
-$TalkFM, $TalkAge, $TalkSpeed = VoiceGender Male, VoiceAge Adult, Rate -2
-$TalkOut = New-Object System.Speech.Synthesis.SpeechSynthesizer.SelectVoiceByHints($TalkFM, $TalkAge, $TalkSpeed)
-PowerShell -Command "$TalkOut.Speak('$SpeechA $wort');"
+$TalkFM, $TalkAge = "Male", "Adult"
+$TalkOut = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$TalkOut.SelectVoiceByHints($TalkFM, $TalkAge)
+$TalkOut.Rate = "-2"
+$TalkOut.Speak("$SpeechA $word")
